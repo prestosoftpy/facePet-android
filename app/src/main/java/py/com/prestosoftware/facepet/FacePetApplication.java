4053e4bde53f0adb1a5296ff218bda5b38c5d71b
rorogarcete <oneosmar89@gmail.com>
2018-11-17 16:58:01 -0300

    Commit Inicial

---
+++ app/src/main/java/py/com/prestosoftware/facepet/FacePetApplication.java
@@ -0,0 +1,15 @@
+package py.com.prestosoftware.facepet;
+
+import android.app.Application;
+
+public class FacePetApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // crashlitycs - > Twitter Crashlitycs
+        // firebase Cloud Messaging -> FCM
+        // Base de Datos Local -> Realm.io
+    }
+}
---
+++ app/src/main/java/py/com/prestosoftware/facepet/MainActivity.java
@@ -0,0 +1,75 @@
+package py.com.prestosoftware.facepet;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.message) TextView mTextMessage;
+    @BindView(R.id.navigation) BottomNavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    // Local Methods
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.title_account);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+}
